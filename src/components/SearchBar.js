847101dd28451172fc066ae859c303bee498bf0b
Ross Hazelett <rhhazelett@gmail.com>
2020-07-11 16:02:16 -0400

    changed to functional components, useState hooks, everything is working, passing the state up to the App component and down to the GUI component is next

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-class SearchBar extends Component {
-  state = {
-    searchString: "",
-    currentPokemon: {},
+const SearchBar = (props) => {
+  // Sets SearchBar state
+  const [searchString, setSearchString] = useState("");
+  const [currentPokemon, setCurrentPokemon] = useState({});
+
+  // Takes data back from api call, sets the currentPokemon in the state
+  const handleData = (data) => {
+    const pokemon = {
+      name: data.name,
+      id: data.id,
+      height: data.height,
+      weight: data.weight,
+      imageUrl: data.species.url,
+    };
+
+    setCurrentPokemon(pokemon);
+    console.log("Pokemon =", pokemon);
   };
 
-  onChange = (e) => {
-    this.setState({ searchString: e.target.value });
+  // onChange of search bar input
+  const onChange = (e) => {
+    setSearchString(e.target.value);
   };
 
-  onSubmit = (e) => {
+  // onSubmit of search form
+  const onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.searchString);
+    console.log(searchString);
     axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${this.state.searchString}`)
+      .get(`https://pokeapi.co/api/v2/pokemon/${searchString}`)
       .then((res) => {
         console.log(res.data);
-        this.setState({ currentPokemon: res.data });
+        handleData(res.data);
       })
       .catch((err) => {
         // sorry, pokemon not found
@@ -26,25 +41,28 @@ class SearchBar extends Component {
       });
   };
 
-  render() {
-    let searchForm = (
-      <form className="form" id="searchForPokemon" onSubmit={this.onSubmit}>
-        <input
-          type="text"
-          className="input"
-          id="addInput"
-          placeholder="Seach for Pokemon"
-          value={this.state.searchString}
-          onChange={this.onChange}
-        />
-        <button className="button" onClick={this.searchPokemon}>
-          Search
-        </button>
-      </form>
-    );
+  // TODO: reset the search input and state after submit
+  // TODO: consider making an api call on every input change
 
-    return searchForm;
-  }
-}
+  // JSX to be returned
+  let searchForm = (
+    <form className="form" id="searchForPokemon" onSubmit={onSubmit}>
+      <input
+        type="text"
+        className="input"
+        id="addInput"
+        placeholder="Seach for Pokemon"
+        value={searchString}
+        onChange={onChange}
+      />
+      <button className="button" type="submit">
+        Search
+      </button>
+    </form>
+  );
+
+  // RETURN JSX
+  return searchForm;
+};
 
 export default SearchBar;

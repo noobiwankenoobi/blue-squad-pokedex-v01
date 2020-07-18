c49b2798d84c6ca343433ce196524d1344fd0c1e
Ross Hazelett <rhhazelett@gmail.com>
2020-07-18 16:39:32 -0400

    added slightly different colors on pokedexcard text, styling smaller screens on next commit, then DONE

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="footer-msg-wrapper">
         <h5 className="footer-text">
           This app proudly
-          <br /> runs on the
+          <br /> powered by the
         </h5>
         <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">
           <img src={require("../img/poke-api.png")} alt="pokeapi"></img>
